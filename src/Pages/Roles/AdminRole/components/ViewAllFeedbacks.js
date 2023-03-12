b97b3e94b2efe1b933fac71f7845892078cad485
uttamsaha <uttamsaha982@live.com>
2023-03-12 13:29:53 +0600

    updated

--- src/Pages/Roles/AdminRole/components/ViewAllFeedbacks.js
+++ src/Pages/Roles/AdminRole/components/ViewAllFeedbacks.js
@@ -36,7 +36,7 @@ const ViewAllFeedbacks = () => {
         // console.log("All courses,",allCourses);
     },[])
     return (
-        <div className='bg-[#F5F5F5] calc-height rounded-b-3xl'>
+        <div className='bg-[#F5F5F5] min-h-screen rounded-b-3xl'>
         <p className='text-white text-2xl mb-10 font-bold bg-[#039BE5] h-24 flex items-center rounded-t-xl'><VscPreview className='ml-5 mr-3 w-6 h-6'/>View Feedbacks</p>
         <div>
        <div class="container mx-auto px-4 sm:px-8">
