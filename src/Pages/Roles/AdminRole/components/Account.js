a182d3279aff6313c6fd362332f3b5768c3b1586
uttamsaha <uttamsaha982@live.com>
2023-03-12 11:28:00 +0600

    bug fixed

--- src/Pages/Roles/AdminRole/components/Account.js
+++ src/Pages/Roles/AdminRole/components/Account.js
@@ -23,7 +23,7 @@ const Account = () => {
             <p className='text-white text-2xl mb-10 font-bold bg-[#039BE5] h-24 flex items-center rounded-t-xl'><VscGitPullRequestCreate className='ml-5 mr-3 w-6 h-6'/>Create Account</p>
             <div className='w-[325px] md:w-[600px] mx-auto'>
             <p className='w-full text-center text-[#b56a00] text-md  py-3 border-l-8 border-[#F0AD4E]  mb-8 bg-[#F4EEE4] rounded-md mx-auto'><b className="font-bold info-size">Info: </b>Please Fill up the form below correctly to create new user accounts. </p>
-                <form onClick={handleLogin} className='flex flex-col items-center bg-white py-10 rounded-xl shadow-sm'>
+                <form onSubmit={handleLogin} className='flex flex-col items-center bg-white py-10 rounded-xl shadow-sm'>
                     <div className='flex justify-start w-full ml-8 md:ml-[100px]'>
                     <label htmlFor="#" className='font-bold'>Username: </label>
                     </div>
